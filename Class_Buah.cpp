a00b8ec68111cdf813ad572d25c1662eb727bfdf
HarisIrwansyah <32352423+HarisIrwansyah@users.noreply.github.com>
2018-01-17 09:06:55 +0800

    menentukan keterangan objek class buah
    deklarasikan string class buah

--- Class_Buah.cpp
+++ Class_Buah.cpp
@@ -23,4 +23,28 @@ void buah::kupas(string luar){
 int main(){
 	buah buahMangga, buahKelapa, buahNaga, buahPepaya;
 	
+	buahMangga.nama   = "Mangga";
+	buahMangga.warna  = "Hijau";
+	buahMangga.rasa   = "Asam";
+	buahMangga.bentuk = "Lonjong";
+	buahMangga.kulit  = "Lunak";
+	
+	buahKelapa.nama   = "Kelapa";
+	buahKelapa.warna  = "Putih";
+	buahKelapa.rasa   = "Hambar";
+	buahKelapa.bentuk = "Bulat";
+	buahKelapa.kulit  = "Keras";
+	
+	buahNaga.nama   = "Naga";
+	buahNaga.warna  = "Merah";
+	buahNaga.rasa   = "Manis";
+	buahNaga.bentuk = "Oval";
+	buahNaga.kulit  = "Lunak";
+	
+	buahPepaya.nama   = "Pepaya";
+	buahPepaya.warna  = "Orange";
+	buahPepaya.rasa   = "Manis";
+	buahPepaya.bentuk = "Lonjong";
+	buahPepaya.kulit  = "Lunak";
+	
 }
